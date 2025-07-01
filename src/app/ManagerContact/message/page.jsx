b0f1f6ca3eef4949ca612f45193db90009a6ccd1
bounmy <bounmyvanhthongpheng09@gmail.com>
2025-07-01 09:40:35 +0700

    fixed

--- src/app/ManagerContact/message/page.jsx
+++ src/app/ManagerContact/message/page.jsx
@@ -110,7 +110,7 @@ export default function MessageViewPage() {
               </label>
               {imagePreview && (
                 <button
-                  onClick={handleRemoveImage}
+                  // onClick={handleRemoveImage}
                   className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
                 >
                   ລຶບຮູບພາບ
