97e6b77c60a534bec2bb924b8c6d66e2d66b2852
bounmy <bounmyvanhthongpheng09@gmail.com>
2025-07-01 10:12:13 +0700

    fixed 02

--- src/app/ManagerContact/message/page.jsx
+++ src/app/ManagerContact/message/page.jsx
@@ -98,7 +98,7 @@ export default function MessageViewPage() {
               <input
                 type="file"
                 accept="image/*"
-                onChange={handleImageChange}
+                // onChange={handleImageChange}
                 className="hidden"
                 id="upload-image"
               />
